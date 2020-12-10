b216408d2f35868dbc6e46de4944d520e3261520
haruu153 <75714515+haruu153@users.noreply.github.com>
2020-12-10 16:44:03 +0900

    Create main.js

---
+++ first/script/main.js
@@ -0,0 +1,14 @@
+ <script>
+  let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+  
+ function setUserName() { 
+   let myName = prompt('あなたの名前を入力してください。');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla はすばらしいよ、' + myName;
+}
+  myButton.onclick = function() {
+  setUserName();
+}
+  
+    </script>
