faf951afcdc6d9d4ba84854837e2cd5212fbb9c5
haruu153 <75714515+haruu153@users.noreply.github.com>
2020-12-15 21:34:59 +0900

    Update main.js

--- first/script/main.js
+++ first/script/main.js
@@ -1,17 +1,3 @@
-
-  let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
-  
- function setUserName() { 
-   let myName = prompt('あなたの名前を入力してください。');
-  localStorage.setItem('name', myName);
-  myHeading.textContent = 'Mozilla はすばらしいよ、' + myName;
-}
-
-  myButton.onclick = function() {
-  setUserName();
-}  
-  
 let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
@@ -21,4 +7,24 @@ myImage.onclick = function() {
     } else {
       myImage.setAttribute ('src','images/firefox-icon.png');
     }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('あなたの名前を入力してください。');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla はすばらしいよ、' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla はすばらしいよ、' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
 }
